181c93979a373065180cc056779000347425ad4d
jennysol <jennyssoliver@gmail.com>
2020-11-18 07:56:50 -0300

    Adicionando methods startGame

--- Projeto Matador de Monstros/app.js
+++ Projeto Matador de Monstros/app.js
@@ -3,7 +3,7 @@ new Vue ({
     data: {
         running: false,
         playerLife: 100,
-        monsterLife: 0,
+        monsterLife: 100,
     },
     computed: {
         hasResult() {
@@ -11,7 +11,11 @@ new Vue ({
         }
     },
     methods: {
-        
+        startGame(){
+            this.running = true 
+            this.playerLife = 100
+            this.monsterLife = 100
+        }
     },
     watch: {
         
