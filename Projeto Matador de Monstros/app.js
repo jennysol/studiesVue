d815a631e9a1f63945973a40a304028632b24edd
jennysol <jennyssoliver@gmail.com>
2020-11-18 07:32:37 -0300

    Finalizando painel Result

--- Projeto Matador de Monstros/app.js
+++ Projeto Matador de Monstros/app.js
@@ -2,10 +2,12 @@ new Vue ({
     el: '#app',
     data: {
         playerLife: 100,
-        monsterLife: 100,
+        monsterLife: 0,
     },
     computed: {
-
+        hasResult() {
+            return this.playerLife == 0 || this.monsterLife == 0
+        }
     },
     methods: {
         
