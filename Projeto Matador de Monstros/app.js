0981d64d5adc1348eae9bbd0b647290578726850
jennysol <jennyssoliver@gmail.com>
2020-11-19 08:00:28 -0300

    Adicionando methods healAndHurt e inicializando logs

--- Projeto Matador de Monstros/app.js
+++ Projeto Matador de Monstros/app.js
@@ -4,6 +4,7 @@ new Vue ({
         running: false,
         playerLife: 100,
         monsterLife: 100,
+        logs: [],
     },
     computed: {
         hasResult() {
@@ -17,17 +18,31 @@ new Vue ({
             this.monsterLife = 100
         },
         attack(especial) {
-            this.hurt('playerLife',7, 12 , false)
-            this.hurt('monsterLife',5, 10 , especial)
+            this.hurt('monsterLife',5, 10 , especial, 'Jogador', 'Monstro', 'player')
+            if(this.monsterLife > 0) {
+                this.hurt('playerLife',7, 12 , false, 'Monstro', 'Jogador', 'monster')
+            }
         },
-        hurt(prop, min, max, especial) {
+        hurt(prop, min, max, especial, source, target, cls) {
             const plus = especial ? 5 : 0
             const hurt = this.getRandom(min + plus, max + plus)
             this[prop] = Math.max(this[prop] - hurt, 0)
+            this.registerLog(`${source} atingiu ${target} com ${hurt}.`, cls)
+        },
+        healAndHurt() {
+            this.hurt(10, 15)
+            this.hurt('playerLife', 7, 12, false)
+        },
+        heal(min, max) {
+            const heal = this.getRandom(min,max)
+            this.playerLife = Math.min(this.playerLife + heal, 100)
         },
         getRandom(min, max) {
             const value = Math.random() * (max - min) + min
             return Math.round(value)
+        },
+        registerLog(text, cls) {
+            this.log.unshift({ text, cls })
         }
     },
     watch: {
