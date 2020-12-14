8984545b26b1eee4b1e7eccf03048e29ffdfd697
jennysol <jennyssoliver@gmail.com>
2020-12-14 08:34:04 -0300

    Adicionando Metodos no Event Bus

--- Vue-cli/comunicacao/src/components/UsuarioInfo.vue
+++ Vue-cli/comunicacao/src/components/UsuarioInfo.vue
@@ -36,7 +36,7 @@ export default {
         }
     },
     created() {
-        barramento.$on('idadeMudou', idade => {
+        barramento.quandoIdadeMudar(idade => {
             this.idade = idade
         } )
     },
