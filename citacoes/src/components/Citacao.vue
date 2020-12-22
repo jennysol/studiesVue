a6cfb86a661370fe2cb8d03b39495e63babde1f1
jennysol <jennyssoliver@gmail.com>
2020-12-22 08:02:43 -0300

    Estilizando conteudo com Slots

--- citacoes/src/components/Citacao.vue
+++ citacoes/src/components/Citacao.vue
@@ -1,20 +1,24 @@
 <template>
-    <div class="citacao">
-        <slot></slot>
-    </div>
+  <div class="citacao">
+      <slot></slot>
+  </div>
 </template>
 
 <script>
 export default {
-    props: ['texto']
+  props: ['texto']
 }
 </script>
 
 <style scoped>
-    .citacao {
-        border: 1px solid #DDD;
-        background-color: rgba(0, 0, 0, .1);
-        padding: 25px;
-        max-width: 450px;
-    }
+  .citacao {
+      border: 1px solid #DDD;
+      background-color: rgba(0, 0, 0, .1);
+      padding: 25px;
+
+  }
+  h1 {
+      color: rgb(199, 199, 199);
+  }
+
 </style>
