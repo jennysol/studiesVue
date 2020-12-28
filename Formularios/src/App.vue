31e1d401d96a0cddbd44c51ff97dc3e39a1bb570
jennysol <jennyssoliver@gmail.com>
2020-12-28 09:57:09 -0300

    Trabalhando butons radios

--- Formularios/src/App.vue
+++ Formularios/src/App.vue
@@ -29,9 +29,9 @@
 							v-model="caracteristicas"> Intermitente</span>
 				</Rotulo>
 				<Rotulo nome="Qual produto?">
-					<span class="mr-4"><input type="radio"> Web</span>
-					<span class="mr-4"><input type="radio"> Mobile</span>
-					<span><input type="radio"> Outro</span>
+					<span class="mr-4"><input type="radio" value="web" v-model="produto"> Web</span>
+					<span class="mr-4"><input type="radio" value="mobile" v-model="produto"> Mobile</span>
+					<span><input type="radio" value="outro" v-model="produto"> Outro</span>
 				</Rotulo>
 				<Rotulo nome="Prioridade">
 					<select name="" id="">
@@ -65,11 +65,11 @@
 						</ul>
 					</span>
 				</Rotulo>
-				<Rotulo nome="Marque as Opções">
+				<!-- <Rotulo nome="Marque as Opções">
 					<span>???</span>
-				</Rotulo>
+				</Rotulo> -->
 				<Rotulo nome="Qual produto?">
-					<span>???</span>
+					<span>{{ produto }}</span>
 				</Rotulo>
 				<Rotulo nome="Prioridade">
 					<span>???</span>
@@ -93,6 +93,7 @@ export default {
 		return {
 			mensagem: '',
 			caracteristicas: [],
+			produto: 'web',
 			usuario: {
 				email: '',
 				senha: '',
