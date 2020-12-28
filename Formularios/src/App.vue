bb4126d21383697f646d54a668499d45c20ffe48
jennysol <jennyssoliver@gmail.com>
2020-12-28 10:56:17 -0300

    Submetendo Formulario

--- Formularios/src/App.vue
+++ Formularios/src/App.vue
@@ -2,7 +2,7 @@
 	<div id="app">
 		<h1>Registrar Reclamação</h1>
 		<div class="conteudo">
-			<form class="painel">
+			<form class="painel" v-if="!enviado">
 				<div class="cabecalho">Formulário</div>
 				<Rotulo nome="E-mail">
 					<input type="text" v-model.lazy.trim="usuario.email">
@@ -47,9 +47,9 @@
 					<Escolha v-model="escolha" />
 				</Rotulo>
 				<hr>
-				<button>Enviar</button>
+				<button @click.prevent="enviar">Enviar</button>
 			</form>
-			<div class="painel">
+			<div class="painel" v-else>
 				<div class="cabecalho">Resultado</div>
 				<Rotulo nome="E-mail">
 					<span>{{ usuario.email }}</span>
@@ -112,6 +112,12 @@ export default {
 				idade: 18
 			},
 			escolha: true,
+			enviado: false,
+		}
+	},
+	methods: {
+		enviar(){
+			this.enviado = true
 		}
 	},
 }
