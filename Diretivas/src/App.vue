7afcfe449bc473de569960f577c87b27580c0b80
jennysol <jennyssoliver@gmail.com>
2020-12-31 17:10:32 -0300

    Registrando Diretivas Localmente

--- Diretivas/src/App.vue
+++ Diretivas/src/App.vue
@@ -8,11 +8,33 @@
 		<p v-destaque:fundo.atrasar="'lightblue'">Usando diretiva personalizada</p>
 		<p v-destaque.atrasar="cor">Usando diretiva personalizada</p>
 
+		<p v-destaque-local:fundo.atrasar="'lightblue'">Usando diretiva personalizada</p>
+		<p v-destaque-local.atrasar="cor">Usando diretiva personalizada</p>
+
 	</div>
 </template>
 
 <script>
 export default {
+	components: { },
+	directives: {
+		'destaque-local': {
+			bind(el, binding, vnode) {
+			// el.style.backgroundColor = 'lightgreen'
+
+			let atraso = 0
+			if(binding.modifiers['atrasar']) atraso = 3000
+			
+			setTimeout(() => {
+				if(binding.arg === 'fundo'){
+					el.style.backgroundColor = binding.value
+				} else {
+					el.style.color = binding.value
+				}
+			}, atraso)
+	}
+		}
+	},
 	data() {
 		return {
 			cor: 'red'
