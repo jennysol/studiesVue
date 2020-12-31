ea7e0a348b351fbaf29e18d108901460a9c98502
jennysol <jennyssoliver@gmail.com>
2020-12-31 17:32:39 -0300

    Usando Multiplos MOdificadores

--- Diretivas/src/App.vue
+++ Diretivas/src/App.vue
@@ -9,7 +9,7 @@
 		<p v-destaque.atrasar="cor">Usando diretiva personalizada</p>
 
 		<p v-destaque-local:fundo.atrasar="'lightblue'">Usando diretiva personalizada</p>
-		<p v-destaque-local.atrasar="cor">Usando diretiva personalizada</p>
+		<p v-destaque-local.alternar="cor">Usando diretiva personalizada</p>
 
 	</div>
 </template>
@@ -20,18 +20,32 @@ export default {
 	directives: {
 		'destaque-local': {
 			bind(el, binding, vnode) {
-			// el.style.backgroundColor = 'lightgreen'
-
-			let atraso = 0
-			if(binding.modifiers['atrasar']) atraso = 3000
-			
-			setTimeout(() => {
-				if(binding.arg === 'fundo'){
-					el.style.backgroundColor = binding.value
-				} else {
-					el.style.color = binding.value
+			const aplicarCor = cor => {
+					if(binding.arg === 'fundo'){
+						el.style.backgroundColor = cor
+					} else {
+						el.style.color = cor
+					}
 				}
-			}, atraso)
+
+				let atraso = 0
+        if(binding.modifiers['atrasar']) atraso = 3000
+
+        const cor1 = binding.value
+        const cor2 = 'purple'
+        let corAtual = cor1
+        
+				
+				setTimeout(() => {
+          if(binding.modifiers['alternar']) {
+            setInterval(() => { 
+              corAtual = corAtual === cor1 ? cor2 : cor1
+              aplicarCor(corAtual)
+            }, 1000)
+          } else {
+            aplicarCor(binding.value)
+          }
+				}, atraso)
 	}
 		}
 	},
