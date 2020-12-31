988615efe6670f05783a89784e90096a37c0f4d6
jennysol <jennyssoliver@gmail.com>
2020-12-31 17:04:50 -0300

    Resumo sobre Diretivas Personalizadas

--- Diretivas/src/App.vue
+++ Diretivas/src/App.vue
@@ -5,8 +5,8 @@
 		<p v-text="'Usando diretiva v-text'"></p>
 		<p v-html="'Usando diretivas <strong>v-thml</strong>'"></p>
 		<hr>
-		<p v-destaque:fundo="'lightblue'">Usando diretiva personalizada</p>
-		<p v-destaque="cor">Usando diretiva personalizada</p>
+		<p v-destaque:fundo.atrasar="'lightblue'">Usando diretiva personalizada</p>
+		<p v-destaque.atrasar="cor">Usando diretiva personalizada</p>
 
 	</div>
 </template>
