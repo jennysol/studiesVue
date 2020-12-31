92e3b5fbb5991b6c5a9b67f597bc5b6f8f15e380
jennysol <jennyssoliver@gmail.com>
2020-12-31 16:18:15 -0300

    Criando uma diret

--- Diretivas/src/main.js
+++ Diretivas/src/main.js
@@ -6,6 +6,7 @@ Vue.config.productionTip = false
 Vue.directive('destaque', {
 	bind(el, binding, vnode) {
 		el.style.backgroundColor = 'lightgreen'
+		el.style.backgroundColor = binding.value
 	}
 })
 
