a84ea476f5665e850e570e6e007971665683bc03
jennysol <jennyssoliver@gmail.com>
2020-12-31 16:13:59 -0300

    Criando uma diretiva simples

--- Diretivas/src/App.vue
+++ Diretivas/src/App.vue
@@ -4,6 +4,8 @@
 		<hr>
 		<p v-text="'Usando diretiva v-text'"></p>
 		<p v-html="'Usando diretivas <strong>v-thml</strong>'"></p>
+		<hr>
+		<p v-destaque>Usando diretiva personalizada</p>
 	</div>
 </template>
 
