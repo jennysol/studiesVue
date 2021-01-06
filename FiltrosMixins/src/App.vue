8dac8e490b4fabbc72912c989a7b5ef7b1ad95d9
jennysol <jennyssoliver@gmail.com>
2021-01-06 07:26:03 -0300

    Criando um Filtro Local

--- FiltrosMixins/src/App.vue
+++ FiltrosMixins/src/App.vue
@@ -1,12 +1,27 @@
 <template>
 	<div id="app">
 		<h1>Filtros & Mixins</h1>
+		<hr>
+		<p>{{ cpf | cpf }}</p>
 	</div>
 </template>
 
 <script>
 export default {
-
+	filters: {
+		cpf(valor) {
+			const arr = valor.split('') //Adicionando um array para cada posição
+			arr.splice(3, 0, '.')
+			arr.splice(7, 0, '.')
+			arr.splice(11, 0, '-')
+			return arr.join('') //Retornando array como string
+		}
+	},
+	data() {
+		return {
+			cpf: '60070080090',
+		}
+	}
 }
 </script>
 
