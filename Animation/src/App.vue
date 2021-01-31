757351d9207ab3d6c3ef971e6899e7f231af88a3
jennysol <jennyssoliver@gmail.com>
2021-01-31 08:26:33 -0300

    Misturando as Propriedades transition e animation

--- Animation/src/App.vue
+++ Animation/src/App.vue
@@ -9,7 +9,7 @@
 			<b-alert variant="info" show v-if="exibir">{{ msg }}</b-alert>
 		</transition>
 
-    <transition name="slide">
+    <transition name="slide" type="animation">
 			<b-alert variant="info" show v-if="exibir">{{ msg }}</b-alert>
 		</transition>
 	</div>
@@ -59,9 +59,16 @@ export default {
 
 .slide-enter-active {
   animation: slide-in 2s ease;
+  transition: opacity 2s;
 }
 
 .slide-leave-active {
   animation: slide-out 2s ease;
+  transition: opacity 2s;
 }
+
+.slide-enter, .slide-leave-to  {
+	opacity: 0;
+}
+
 </style>
