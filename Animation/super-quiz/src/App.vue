d8f09ed853d4b7b61378ee25da82d2401ad2fb4c
jennysol <jennyssoliver@gmail.com>
2021-02-06 17:08:11 -0300

    Implementando animaçoes

--- Animation/super-quiz/src/App.vue
+++ Animation/super-quiz/src/App.vue
@@ -1,14 +1,16 @@
 <template>
 	<div id="app">
 		<h1>Super Quiz</h1>
-		<Question v-if="questionMode"
+		<transition name="flip" mode="out-in">
+			<Question v-if="questionMode"
 			:question="questions[currentQuestion]"
 			@answered="showResult"
-		/>
-		<Result v-else
-			:result="result"
-			@confirmed="nextQuestion"
-		/>
+			/>
+			<Result v-else
+				:result="result"
+				@confirmed="nextQuestion"
+			/>
+		</transition>
 	</div>
 </template>
 
