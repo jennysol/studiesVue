8f818e609aca221ec8fc23b95287a515178bfda6
jennysol <jennyssoliver@gmail.com>
2021-02-08 21:29:19 -0300

    Criando Form

--- HTTP/http-exercicios/src/App.vue
+++ HTTP/http-exercicios/src/App.vue
@@ -1,11 +1,53 @@
 <template>
 	<div id="app" class="container">
 		<h1>HTTP com Axios</h1>
+		<b-card>
+			<b-form-group label="Nome:">
+				<b-form-input 
+					type="text" 
+					size="lg" 
+					v-model="user.name"
+					placeholder="Infome o Nome"
+				>
+				</b-form-input>
+			</b-form-group>
+
+			<b-form-group label="Email:">
+				<b-form-input 
+					type="text" 
+					size="lg" 
+					v-model="user.email"
+					placeholder="Informe o Email"
+				>
+				</b-form-input>
+			</b-form-group>
+			<hr>
+			<b-button
+				@click="save"
+				size="lg"
+				variant="primary"
+			>
+				Salvar
+			</b-button>
+		</b-card>
 	</div>
 </template>
 
 <script>
 export default {
+	data() {
+		return {
+			user : {
+				name: '',
+				email:'',
+			}
+		}
+	},
+	methods: {
+		save() {
+			console.log(this.user);
+		}
+	}
 	// created() {
 	// 	this.$http.post('usuarios.json', {
 	// 		name: 'Jennifer',
