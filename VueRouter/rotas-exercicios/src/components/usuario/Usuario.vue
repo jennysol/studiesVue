a6b1d3826d833fb88c9e32f31a6c502c9b044f18
jennysol <jennyssoliver@gmail.com>
2021-02-17 21:35:34 -0300

    Lendo e usando parametros de rotas

--- VueRouter/rotas-exercicios/src/components/usuario/Usuario.vue
+++ VueRouter/rotas-exercicios/src/components/usuario/Usuario.vue
@@ -2,18 +2,24 @@
     <div class="usuario">
         <h2>Usuário</h2>
         <hr>
+        <p><strong>Código:</strong> {{ id }}</p>
         <button sucesso @click="goStart">Voltar</button>
     </div>
 </template>
 
 <script>
 export default {
-    methods: {
-        goStart() {
-          // this.$router.push('/')
-          this.$router.push({ path: '/' })
-        }
+  data() {
+    return {
+      id: this.$route.params.id
     }
+  },
+  methods: {
+      goStart() {
+        // this.$router.push('/')
+        this.$router.push({ path: '/' })
+      }
+  }
 }
 </script>
 
