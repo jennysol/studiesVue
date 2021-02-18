b1041bce53cb1122ed02432f0d8c4a3096d616b1
jennysol <jennyssoliver@gmail.com>
2021-02-17 21:26:45 -0300

    Navegando via Codigo

--- VueRouter/rotas-exercicios/src/components/usuario/Usuario.vue
+++ VueRouter/rotas-exercicios/src/components/usuario/Usuario.vue
@@ -1,12 +1,19 @@
 <template>
     <div class="usuario">
         <h2>Usuário</h2>
+        <hr>
+        <button sucesso @click="goStart">Voltar</button>
     </div>
 </template>
 
 <script>
 export default {
-    
+    methods: {
+        goStart() {
+          // this.$router.push('/')
+          this.$router.push({ path: '/' })
+        }
+    }
 }
 </script>
 
