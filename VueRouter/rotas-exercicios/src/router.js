01068d5f8bac7a3df7eb938815b4f0a2ad176420
jennysol <jennyssoliver@gmail.com>
2021-02-22 07:47:08 -0300

    Carregando Rotas Tardiamente

--- VueRouter/rotas-exercicios/src/router.js
+++ VueRouter/rotas-exercicios/src/router.js
@@ -2,12 +2,17 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Inicio from './components/Inicio'
 import Menu from './components/templates/Menu'
-import Usuario from './components/usuario/Usuario'
-import UsuarioLista from './components/usuario/UsuarioLista'
-import UsuarioDetalhe from './components/usuario/UsuarioDetalhe'
-import UsuarioEditar from './components/usuario/UsuarioEditar'
+// import Usuario from './components/usuario/Usuario'
+// import UsuarioLista from './components/usuario/UsuarioLista'
+// import UsuarioDetalhe from './components/usuario/UsuarioDetalhe'
+// import UsuarioEditar from './components/usuario/UsuarioEditar'
 
 Vue.use(Router)
+
+const Usuario  = () => import(/*webpackChunkName: "usuario" */'./components/usuario/UsuarioEditar')
+const UsuarioLista  = () => import(/*webpackChunkName: "usuario" */'./components/usuario/UsuarioLista')
+const UsuarioDetalhe  = () => import(/*webpackChunkName: "usuario" */'./components/usuario/UsuarioDetalhe')
+const UsuarioEditar  = () => import(/*webpackChunkName: "usuario" */'./components/usuario/UsuarioEditar')
 
 const router =  new Router({
     mode: 'history',
